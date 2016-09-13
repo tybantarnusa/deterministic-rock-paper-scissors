c402fcb5da8c9aa972b1b96af276fe8151a4b217
Mgs. M. Thoyib Antarnusa <tybantarnusa@null.net>
2016-09-14 05:57:46 +0700

    Initial project build

---
+++ html/src/com/tybprojekt/drps/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.tybprojekt.drps.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.tybprojekt.drps.MyGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new MyGame();
+        }
+}
